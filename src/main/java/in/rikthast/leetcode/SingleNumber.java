8e4b5ce6256edea26d63cf2b92f86e8551ea2df5
Achal Yash <dev.achalyash@gmail.com>
2024-06-16 00:38:22 +0530

    doc(leetcode-136): Add documentation comment

--- src/main/java/in/rikthast/leetcode/SingleNumber.java
+++ src/main/java/in/rikthast/leetcode/SingleNumber.java
@@ -1,6 +1,9 @@
 package in.rikthast.leetcode;
 
 public class SingleNumber {
+  /** Uses XOR Identity and XOR Negation
+   */
+
   public static int call(int[] nums) {
     int result = 0;
     for(int i: nums) {
